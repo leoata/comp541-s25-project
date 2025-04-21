41229ba68b462132daf830592d4aae82b595486e
leoata <leonardo@atalla.org>
2025-04-20 20:51:15 -0400

    ff

--- main.asm
+++ main.asm
@@ -28,7 +28,7 @@ k:      .word 40         # number of columns
 
 maze: 	
 	.word  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-	.word  1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	.word  1, 2, 0, 0, 3, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 	.word  1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 	.word  1, 0, 5, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 	.word  1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
@@ -88,10 +88,6 @@ main:
     
     la   $s6, maze         # Load address of maze array
     
-    addi $s0, $zero, 64 # preciseBallX aka the true X value multiplied by 64
-    addi $s1, $zero, 64 # preciseBallY aka the true Y value multiplied by 64
-    
-    
     add   $a2, $zero, $zero    # i = 0
     lw    $s2, n               # s2 = number of rows (n)
     lw    $s3, k               # s3 = number of cols (k)
@@ -139,38 +135,39 @@ increment_maze_display_loop:
 ##		- For reading the collision type
 
 first_game_loop:
+    addi $s0, $zero, 64 # preciseBallX aka the true X value multiplied by 64
+    addi $s1, $zero, 64 # preciseBallY aka the true Y value multiplied by 64
     add $s4, $zero, $zero # +1 = speed buff, 0 = nothing, -1 = slow debuff
     add $s5, $zero, $zero # wall breaker uses left
+    addi $a0, $zero, 0
+    addi $a1, $zero, 39
+    addi $a2, $zero, 1
+    jal putChar_atXY
 game_loop:
-	addi $a0, $zero, 0
-	srl $a1, $s0, 6
-	srl $a2, $s1, 6
-	jal putChar_atXY
 
 
 	jal get_accelX
 	move $s6, $v0
 	addi $s6, $s6, -256
-	sub $s6, $zero, $s6
-	
+	sra $s6, $s6, 3
 	
 	jal get_accelY
 	move $s7, $v0
 	addi $s7, $s7, -256
-	sub $s7, $zero, $s7
+	sra $s7, $s7, 3
 
-	addi $s2, $zero, 1
-	beq $s2, $s4, speedup_movement
-	addi $s2, $zero, -1
-	beq $s2, $s4, slowdown_movement
+	#addi $s2, $zero, 1
+	#beq $s2, $s4, speedup_movement
+	#addi $s2, $zero, -1
+	#beq $s2, $s4, slowdown_movement
 after_movement_speed_adjustments:
 	
 	
 	add $s2, $s0, $s7 # targetX = preciseBallX + preciseAccelY (accelY is actually X in game)
 	add $s3, $s1, $s6 # targetY = preciseBallY + preciseAccelX (accelX is actually Y in game)
 	
-	srl $a1, $s2, 6
-	srl $a2, $s3, 6
+	sra $a1, $s2, 6
+	sra $a2, $s3, 6
 	jal getChar_atXY
 	
 	move $s7, $v0
@@ -192,70 +189,70 @@ after_movement_speed_adjustments:
 	beq $s6, $s7, collision_case6
 	
 resume_after_collisions:
-	move $s0, $s2 # preciseBallX = preciseTargetX
-	move $s1, $s3 # preciseBallY = preciseTargetY
-	
-	move $v0, $s0
-	move $v1, $s1
 
-	blt $s0, $zero, ball_x_too_low
-	blt $s1, $zero, ball_y_too_low
 	
-	lw $t2, n
-	lw $t3, k
-	srl $t4, $s0, 6 # ballX
-	srl $t5, $s1, 6 # ballY
-	slt $t4, $t4, $t3 # set $a0 to 0 if preciseBallX/16 is too high
-	slt $t5, $t5, $t2 # set $a1 to 0 if preciseBallY/16 is too high
+#	move $v0, $s2
+#	move $v1, $s3
+
+#	blt $s0, $zero, ball_x_too_low
+after_ball_x_too_low:
+#	blt $s1, $zero, ball_y_too_low
+after_ball_y_too_low:
 	
-	beq $t5, $zero, ball_x_too_high
-	beq $t5, $zero, ball_y_too_high
-	move $s0, $v0
-	move $s1, $v1
-	
-	addi $a0, $zero, 2
-	srl $a1, $s0, 6
-	srl $a2, $s1, 6
-	
-	jal putChar_atXY
-	
-	#addi $a0, $zero, 10
+#	lw $t2, n
+#	lw $t3, k
+#	srl $t4, $s0, 6 # ballX
+#	srl $t5, $s1, 6 # ballY
+#	slt $t4, $t4, $t3 # set $a0 to 0 if preciseBallX/64 is too high
+#	slt $t5, $t5, $t2 # set $a1 to 0 if preciseBallY/64 is too high
 #	
-#	jal pause
+#	beq $t5, $zero, ball_x_too_high
+after_ball_x_too_high:
+#	beq $t5, $zero, ball_y_too_high
+after_ball_y_too_high:
+#	move $s2, $v0
+#	move $s3, $v1
+
+	bne $s0, $s2, show_player_pos
+	bne $s1, $s3, show_player_pos
 	
-	
-	
+resume_after_show_player_pos:
+
 	j game_loop
     #jal 	generateMaze
 
 
+show_player_pos:
+	addi $a0, $zero, 0
+	sra $a1, $s0, 6
+	sra $a2, $s1, 6
+	jal putChar_atXY
+	
+	move $s0, $s2 # preciseBallX = preciseTargetX
+	move $s1, $s3 # preciseBallY = preciseTargetY
+	
+	addi $a0, $zero, 2
+	sra $a1, $s0, 6
+	sra $a2, $s1, 6
+	
+	jal putChar_atXY
+	
+	j resume_after_show_player_pos
         
 collision_case1: # char 1 == wall, try wallbreaker
 	bne $s5, 0, use_wallbreaker_on_full_wall
+collision_case_1_pt2:
 	move $s2, $s0
 	move $s3, $s1
 	j resume_after_collisions
 	
-	
 collision_case4: # char 4 == slow debuff
 	subi $s4, $zero, 1 # set speed buff/debuff val to -1
-	
-	addi $a0, $zero, 0
-	srl $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
-	srl $a2, $s3, 6 # divide preciseBallY by 16 to revert back to screen units
-	
-	jal putChar_atXY
 	
 	j resume_after_collisions
 	
 collision_case3: # char 3 == super speed
 	addi $s4, $zero, 1 # set speed buff/debuff val to +1
-
-	add $a0, $zero, 0
-	srl $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
-	srl $a2, $s3, 6 # divide preciseBallY by 16 to revert back to screen units
-	
-	jal putChar_atXY
 	
 	j resume_after_collisions
 
@@ -263,109 +260,59 @@ collision_case5: # char 5 == wall breaker
 	
 	addi $s5, $zero, 5
 	
-	add $a0, $zero, 0
-	srl $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
-	srl $a2, $s3, 6 # divide preciseBallY by 16 to revert back to screen units
-	
-	jal putChar_atXY
-	
 	j resume_after_collisions
 	
 	
 collision_case6: # damaged wall
 	
 	bne $s5, 0, use_wallbreaker_on_damaged_wall
-	
-	add $a0, $zero, 0
-	srl $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
-	srl $a2, $s3, 6 # divide preciseBallY by 16 to revert back to screen units
-	
-	jal putChar_atXY
+collision_case6_pt2:
 	
 	j resume_after_collisions
 use_wallbreaker_on_damaged_wall:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
-	
 	add $a0, $zero, 0
-	srl $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
-	srl $a2, $s3, 6 # divide preciseBallXY by 16 to revert back to screen units
-	
-	subi $s5, $s5, 1
+	sra $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
+	sra $a2, $s3, 6 # divide preciseBallXY by 16 to revert back to screen units
+
 	jal putChar_atXY
 	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
+	subi $s5, $s5, 1
+	j collision_case6_pt2
 
 use_wallbreaker_on_full_wall:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
-	
 	add $a0, $zero, 6
-	srl $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
-	srl $a2, $s3, 6 # divide preciseBallXY by 16 to revert back to screen units
+	sra $a1, $s2, 6 # divide preciseBallX by 16 to revert back to screen units
+	sra $a2, $s3, 6 # divide preciseBallXY by 16 to revert back to screen units
+	
+	move $s2, $s0
+	move $s3, $s1
 	
 	subi $s5, $s5, 1
 	jal putChar_atXY
 	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
+	j collision_case_1_pt2
 
-# $a0 = preciseBallX, $a1, preciseBallY
-ensure_ball_within_bounds:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
-	
-	move $v0, $a0
-	move $v1, $a1
-	
-	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
 ball_x_too_low:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
+	add $v0, $zero, $zero
 	
-	add $v0, $v0, $zero
-	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
+	j after_ball_x_too_low
 ball_y_too_low:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
+	add $v1, $zero, $zero
 	
-	add $v1, $v1, $zero
-	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
+	j after_ball_y_too_low
 
 ball_x_too_high:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
-
 	add $v0, $zero, $t3
 	subi $v0, $v0, 1
 	sll $v0, $v0, 6 # restore the return value to the correct units
 	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
+	j after_ball_x_too_high
 ball_y_too_high:
-	addi $sp, $sp, -4   # Decrement stack pointer to make room
-	sw   $ra, 0($sp)    # Save the return address on the stack
-
 	add $v1, $zero, $t2
-	subi $v1, $v1, 1
+	addi $v1, $v1, -1
 	sll $v1, $v1, 6 # restore the return value to the correct units
 	
-	lw   $ra, 0($sp)    # Restore the return address
-	addi $sp, $sp, 4    # Restore the stack pointer
-	jr   $ra           # Return to the caller
+	j after_ball_y_too_high
 
 
 speedup_movement:
@@ -374,8 +321,8 @@ speedup_movement:
 	j after_movement_speed_adjustments
 
 slowdown_movement:
-	srl $s6, $s6, 1
-	srl $s7, $s7, 1
+	sra $s6, $s6, 1
+	sra $s7, $s7, 1
 	j after_movement_speed_adjustments
 
 exit_from_main:
